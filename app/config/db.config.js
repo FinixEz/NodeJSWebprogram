2c527f0912c729f2744367b472e615e75cc22e6c
FinixZ <supasan.m13@gmail.com>
2023-10-02 15:24:22 +0700

    First

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports ={
+    // Planetscale
+    // HOST: "aws.connect.psdb.cloud",
+    // USER: "0li36tsbka7o2tepqemf",
+    // PASSWORD: "pscale_pw_NEwmNrVxgWv6clsv9ikYnKOYM6aq4SKtb64bamAWazB",
+    // DB: "mydatabase_api"
+
+    // FreeDb
+    HOST:"sql.freedb.tech",
+    USER:"freedb_testapi",
+    PASSWORD:"m86mv*ae$?BEQ66",
+    DB:"freedb_myapi_db_finix"
+};
